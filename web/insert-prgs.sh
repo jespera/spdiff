e81c6a22985475ae9adb05a60840813b2ec49711
Jesper Andersen <jespera@diku.dk>
2009-03-23 21:30:13 +0100

    Intergrate perl usage in insert-prgs.sh script.

--- web/insert-prgs.sh
+++ web/insert-prgs.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-n=`echo $1 | sed "s/.html/.xhtml/"`
-echo "Copying $1 to $n"
-cp $1 $n
+#n=`echo $1 | sed "s/.html/.xhtml/"`
+#echo "Copying $1 to $n"
+#cp $1 $n
 for i in examples/*.c; do
 				b=`basename $i`
-				sed "s/FILE/$b/" -i insert.sed
+#				sed "s/FILE/$b/" -i insert.sed
 				echo "Inserting program : " $b
-				sed -f insert.sed -i $n
-				sed "s/$b/FILE/" -i insert.sed
+				./insert-prgs.pl $1 $b > tmp.html
+				mv tmp.html $1
+				rm -f tmp.html
+#				sed -f insert.sed -i $n
+#				sed "s/$b/FILE/" -i insert.sed
 done
 
