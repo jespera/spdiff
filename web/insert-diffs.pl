72a7993c063cb2379d8353806047a5b65e657c0e
Jesper Andersen <jespera@diku.dk>
2009-03-24 08:11:47 +0100

    Use diff to show ... diffs in doc.html

---
+++ web/insert-diffs.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+$html_name = $ARGV[0];
+
+open(hfile, $html_name);
+
+while ($line = <hfile>) {
+				# are we on a diff line ?
+				if ($line =~ /<!-- diff (.*) (.*) -->/) {
+					$old = $1;
+					$new = $2;
+					print $line;
+					$line = <hfile>;
+					if ($line =~ /<pre>/) {
+									print $line;
+									$flag = 1;
+									while ($flag == 1) {
+													$line = <hfile>;
+													if ($line =~ /<\/pre>/) {
+																	$flag = 0;
+																	system("diff -u -t examples/$old examples/$new | tail --lines=+4");
+																	print $line;
+													}
+									}
+					} else {
+									print $line;
+					}
+				} else {
+							 print $line;
+			  }	 
+}
